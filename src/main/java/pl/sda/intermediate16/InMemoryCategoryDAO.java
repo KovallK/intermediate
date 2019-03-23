648b462d4921bc7999369aba4f2f079651066c7a
KovallK <k.o.wal85@wp.pl>
2019-03-23 13:38:42 +0100

    init

--- src/main/java/pl/sda/intermediate16/InMemoryCategoryDAO.java
+++ src/main/java/pl/sda/intermediate16/InMemoryCategoryDAO.java
@@ -5,18 +5,37 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
-public class InMemoryCategoryDAO {                  //klasa do stworzenia listy kategorii ,DAO znaczy Data Accrss Object
+public class InMemoryCategoryDAO {//klasa do stworzenia listy kategorii ,DAO znaczy Data Accrss Object
 
-    private void loadCategoriesFromFile() {
+   private List<Category> categoryList=new ArrayList<>();
+    private List<String> loadCategoriesFromFile() {
         ClassLoader classLoader = this.getClass().getClassLoader();
         URI uri = null;//wczytywanie pliku za pomocą klasy ClassLoader,Uri to adres,ścieżka do pliku/zasobu
         try {
             uri = classLoader.getResource("kategorie.txt").toURI();
             List<String> lines = Files.readAllLines(Paths.get(uri));
+            return lines;
         } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
+        return new ArrayList<>();
+    }
+
+    private void initializeCategories(){
+        List<String> categoriesFromFile = loadCategoriesFromFile();
+         AtomicInteger counter=new AtomicInteger(1);
+        List<Category> categories = categoriesFromFile.stream()
+                .map(s -> {
+                    Category category = new Category();
+                    category.setCategoryName(s);
+                    category.setId(counter.getAndIncrement());
+                    return category;
+                })
+                .collect(Collectors.toList());
     }
 }
